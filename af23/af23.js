b9bdddcb8b39c93e6ac1287c13e71b590e5000f2
RealBen9 <bjdstephens@gmail.com>
2023-03-31 23:57:53 +0100

    april fools!

--- af23/af23.js
+++ af23/af23.js
@@ -2,11 +2,12 @@ snow = document.querySelectorAll("div")[0]
 snow.style.rotate = '270deg'
 af23 = Math.floor(Math.random() * 25)
 if (af23 == 0) {
-    af23 = Math.floor(Math.random() * 6)
+    af23 = Math.floor(Math.random() * 7)
     if (af23 == 0) {window.location.replace('https://www.youtube.com/watch?v=dQw4w9WgXcQ')}
     else if (af23 == 1) {window.location.replace('af23/crates.html')}
     else if (af23 == 2) {window.location.replace('af23/pixel_draw.html')}
     else if (af23 == 3) {window.location.replace('af23/gateway.html')}
     else if (af23 == 4) {window.location.replace('af23/rotary_moment.html')}
+    else if (af23 == 4) {window.location.replace('af23/gravity.html')}
     else {window.location.replace('af23/bsod.html')}
 }
