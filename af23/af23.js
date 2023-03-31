7392244fbca37d62e0dc36b2f5dd40c127476f8f
RealBen9 <bjdstephens@gmail.com>
2023-03-31 23:31:43 +0100

    april fools!

---
+++ af23/af23.js
@@ -0,0 +1,12 @@
+snow = document.querySelectorAll("div")[0]
+snow.style.rotate = '270deg'
+af23 = Math.floor(Math.random() * 25)
+if (af23 == 0) {
+    af23 = Math.floor(Math.random() * 6)
+    if (af23 == 0) {window.location.replace('https://www.youtube.com/watch?v=dQw4w9WgXcQ')}
+    else if (af23 == 1) {window.location.replace('af23/crates.html')}
+    else if (af23 == 2) {window.location.replace('af23/pixel_draw.html')}
+    else if (af23 == 3) {window.location.replace('af23/gateway.html')}
+    else if (af23 == 4) {window.location.replace('af23/rotary_moment.html')}
+    else {window.location.replace('af23/bsod.html')}
+}
