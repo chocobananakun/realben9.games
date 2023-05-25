51ff830dcbe8a153d85b855d2c35495bea76d205
RealBen9 <bjdstephens@gmail.com>
2023-05-25 16:20:53 +0100

    fix april fools archive

--- af23/af23.js
+++ af23/af23.js
@@ -4,10 +4,10 @@ af23 = Math.floor(Math.random() * 25)
 if (af23 == 0) {
     af23 = Math.floor(Math.random() * 7)
     if (af23 == 0) {window.location.replace('https://www.youtube.com/watch?v=dQw4w9WgXcQ')}
-    else if (af23 == 1) {window.location.replace('af23/crates.html')}
-    else if (af23 == 2) {window.location.replace('af23/pixel_draw.html')}
-    else if (af23 == 3) {window.location.replace('af23/gateway.html')}
-    else if (af23 == 4) {window.location.replace('af23/rotary_moment.html')}
-    else if (af23 == 4) {window.location.replace('af23/gravity.html')}
-    else {window.location.replace('af23/bsod.html')}
+    else if (af23 == 1) {window.location.replace('crates.html')}
+    else if (af23 == 2) {window.location.replace('pixel_draw.html')}
+    else if (af23 == 3) {window.location.replace('gateway.html')}
+    else if (af23 == 4) {window.location.replace('rotary_moment.html')}
+    else if (af23 == 5) {window.location.replace('gravity.html')}
+    else {window.location.replace('bsod.html')}
 }
